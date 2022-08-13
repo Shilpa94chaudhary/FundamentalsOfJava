500015717d48c5b9b9f75d0ea8ded1c5e4746fbd
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2022-08-13 17:51:21 +0530

    java file

--- src/Patterns/ParallelogramPattern.java
+++ src/Patterns/ParallelogramPattern.java
@@ -1,10 +1,16 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class ParallelogramPattern {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner s = new Scanner(System.in);
+
+		int n, i=1, space = 0, j;
+
+		n = s.nextInt();
 		
 	}
 
