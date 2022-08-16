79726e05cd38afac72a91785cc0d2ed029dfec2d
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2022-08-16 10:32:23 +0530

    New program for binary number to decimal number conversion

--- src/OperatorsAndForLoop/BinaryToDecimal.java
+++ src/OperatorsAndForLoop/BinaryToDecimal.java
@@ -1,10 +1,33 @@
 package OperatorsAndForLoop;
 
+import java.util.Scanner;
+
 public class BinaryToDecimal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner s = new Scanner(System.in);
+		
+		int i,j,numBinary, numDecimal=0, rem, numPoweri;
+		
+		numBinary = s.nextInt();
+		
+		for(i=0;;i++ ) {
+			
+			numPoweri = 1;
+			for(j=i;j>0;j--) {
+				numPoweri *= 2;
+				
+			}
+			rem = numBinary % 10;
+			numDecimal += (rem * numPoweri);
+			numBinary = numBinary/10;
+			if(numBinary == 0) {
+				break;
+			}
+		}
+		System.out.println(numDecimal);		
+				
 	}
 
 }
