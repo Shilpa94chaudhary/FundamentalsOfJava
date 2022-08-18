2357aeef11a0f6f7c63cedd45b8b506adfa1d0a8
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2022-08-18 22:53:12 +0530

    Update in programs

--- src/FunctionsAndScope/CheckPrime.java
+++ src/FunctionsAndScope/CheckPrime.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 public class CheckPrime {
 	
 	public static boolean checkPrime(int n) {
-		boolean isPrime = true;
 		
 		for(int i = 2; i <= n/2 ; i++) {
 			if(n % i == 0) {
-				isPrime = false;
-				break;
+				return false;
 			}
 		}
 		
-		return isPrime;
+		return true;
 	}
 
 	public static void main(String[] args) {
--- src/Test1/CheckArmstrong.java
+++ src/Test1/CheckArmstrong.java
@@ -17,23 +17,45 @@ public class CheckArmstrong {
 //		true
 		
 		
+//		Scanner s = new Scanner(System.in);
+//		int num, newNum = 0, temp;
+//		
+//		num = s.nextInt();
+//		temp = num;
+//		while(temp != 0) {
+//			int digit = temp % 10;
+//			newNum = newNum + (digit * digit * digit);
+//			temp = temp/10;
+//			
+//		}
+//		
+//		if (num == newNum) {
+//			System.out.println(true);
+//		}else {
+//			System.out.println(false);
+//		}
+		
 		Scanner s = new Scanner(System.in);
-		int num, newNum = 0, temp;
-		
-		num = s.nextInt();
-		temp = num;
-		while(temp != 0) {
-			int digit = temp % 10;
-			newNum = newNum + (digit * digit * digit);
-			temp = temp/10;
-			
-		}
-		
-		if (num == newNum) {
-			System.out.println(true);
-		}else {
-			System.out.println(false);
-		}
+        int n = s.nextInt();
+        int temp, sum=0;
+        
+        
+        for(temp = n; temp > 0;){
+            int prod = 1;
+            for (int temp1 = n; temp1 >0; ){
+                prod = prod * (temp%10);
+                temp1 = temp1/10;
+                
+            }
+            sum += prod;
+            temp = temp/10;
+        }
+
+        if(sum == n) {
+        	System.out.println(true);
+        }else {
+        	System.out.println(false);
+        }
 
 	}
 
