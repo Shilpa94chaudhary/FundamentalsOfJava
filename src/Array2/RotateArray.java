f673a74a15dc555893c2d109589b17bb06030d84
Shilpa Chaudhary <shilpa.94chaudhary@gmail.com>
2022-09-29 10:38:35 +0530

    Update in Rotate Array

--- src/Array2/RotateArray.java
+++ src/Array2/RotateArray.java
@@ -3,7 +3,12 @@ package Array2;
 public class RotateArray {
 
 	public static void rotate(int[] arr, int d) {
-    	int n = arr.length;
+		int n = arr.length; 
+		if(d>n){
+	            while(d>n){
+	                d=d-n;
+	            }
+	        }
     	int[] tempArr = new int[d];
     	for(int i = 0 ; i<d ; i++) {
     		tempArr[i]=arr[i];
@@ -30,6 +35,10 @@ public class RotateArray {
 		printArray(arr);
 		rotate(arr,3);
 		printArray(arr);
+		
+		int arr2[] = {25, 6, 20, 55, 69, 5, 50, 63, 61, 41, 87, 80, 2, 96, 77, 70, 12, 43, 31, 8, 64, 41, 68, 18, 95, 79, 52, 74, 1, 98, 3, 26, 3, 74, 32, 23, 79, 81, 37, 39, 21, 24, 18, 22, 71, 47, 44};
+		rotate(arr2, 132);
+		printArray(arr2);
 	}
 
 }
