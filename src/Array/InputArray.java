dd50653c02e9db3e75a5a93c15ec88a68cd3cced
ShilpaGamezop <shilpa@gamezop.co>
2022-11-12 22:31:38 +0530

    Delete Bin files

--- src/Array/InputArray.java
+++ src/Array/InputArray.java
@@ -6,6 +6,7 @@ public class InputArray {
 
 	public static void main(String[] args) {
 		
+		@SuppressWarnings("resource")
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter the size of array: ");
 		int n = s.nextInt();
